39f74277fc014a7e7dc9c3c0a53e074b88b2d52a
AnsQns <taiwan031614@gmail.com>
2024-05-09 23:31:32 +0800

    A

--- main.js
+++ main.js
@@ -13,3 +13,37 @@ function AddPlayerName(index){
 //對局開始按鈕
 document.getElementById("button").addEventListener("click",BasicScore);
 document.getElementById("button1").addEventListener("click",AddPlayerName);
+
+//翻數符數
+function calculate(){
+    //取得html文件
+    var fanNumber =parseInt(document.getElementById("fanNumber").value);
+    var fuNumber = parseInt(document.getElementById("fuNumber").value);
+    
+    //計算翻數
+    var fuNumber = 0;
+    if (fanNumber === 1){
+
+    } else if (fanNumber === 2){
+
+    } else if (fanNumber === 3){
+
+    } else if (fanNumber === 4){
+
+    } else if (fanNumber === 5){
+        console.log("滿貫");
+    } else if (fanNumber === 6 || fanNumber === 7){
+        console.log("倍滿");
+    } else if (fanNumber === 8 || fanNumber === 10){
+        console.log("跳滿");
+    } else if (fanNumber === 11 || fanNumber === 12){
+        console.log("三倍滿");
+    } else if (fanNumber >=13){
+        console.log("役滿");
+    }else{
+        console.log("重新輸入");
+    }
+    //顯示符數test
+    document.getElementById("result").innerText = fanNumber + "翻" + fuNumber + "符";
+    
+}
