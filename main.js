465ab10f15da3418bb6bb41252ba0b611e7f87cc
AnsQns <taiwan031614@gmail.com>
2024-05-13 21:38:12 +0800

    B

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-//基本點
-function BasicScore(){
-    const Score = 25000;
-    console.log("25000")
+//網頁
+function BasicGame(){
+    window.location.href = "MJ2.html";
 }
 //紀錄玩家名稱
 function PlayerNames(){
@@ -14,39 +13,54 @@ function PlayerNames(){
 }
 
 //對局開始&紀錄按鈕
-document.getElementById("button").addEventListener("click",BasicScore);
+document.getElementById("button").addEventListener("click",BasicGame);
 document.getElementById("button1").addEventListener("click",PlayerNames);
 
-//翻數符數
+//飜數符數
 function calculate(){
-    //取得html文件
+    //取得html文件值
     var fanNumber =parseInt(document.getElementById("fanNumber").value);
     var fuNumber = parseInt(document.getElementById("fuNumber").value);
+    var result1Eleent = parseInt(document.getElementById("result1"));
+    var result2Eleent = parseInt(document.getElementById("result2"));
     
-    //計算翻數
-    var fuNumber = 0;
+    //顯示飜數符數
+    result1Eleent.innerText = fanNumber + "翻" + fuNumber + "符";
+    
+    //計算飜數
     if (fanNumber === 1){
-
+        calculate1();
     } else if (fanNumber === 2){
-
+        calculate1();
     } else if (fanNumber === 3){
-
+        calculate1();
     } else if (fanNumber === 4){
-
+        calculate1();
     } else if (fanNumber === 5){
-        console.log("滿貫");
+        result2Eleent.innerText ="滿貫";
     } else if (fanNumber === 6 || fanNumber === 7){
-        console.log("倍滿");
+        result2Eleent.innerText = "倍滿";
     } else if (fanNumber === 8 || fanNumber === 10){
-        console.log("跳滿");
+        result2Eleent.innerText = "跳滿";
     } else if (fanNumber === 11 || fanNumber === 12){
-        console.log("三倍滿");
+        result2Eleent.innerText = "三倍滿";
     } else if (fanNumber >=13){
-        console.log("役滿");
+        result2Eleent.innerText = "役滿";
     }else{
-        console.log("重新輸入");
+        result2Eleent.innerText = "重新輸入";
     }
-    //顯示符數test
-    document.getElementById("result").innerText = fanNumber + "翻" + fuNumber + "符";
+        function calculate1(){
+        //取得html文件值
+         var fanNumber =parseInt(document.getElementById("fanNumber").value);
+         var fuNumber = parseInt(document.getElementById("fuNumber").value);
+
+        //定義符數計算
+         var FU1 = 6 * fuNumber * Math.pow(2, fanNumber + 2);
+         var FU2 = 4 * fuNumber * Math.pow(2, fanNumber + 2);
+
+        //顯示點數
+         var result2Eleent = document.getElementById("result2");
+         result2Eleent.innerText = FU1 + "點" + FU2 +"點";
+        }
     
 }
